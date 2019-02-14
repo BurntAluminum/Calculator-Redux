3e495bb8cb400f8b35207fe8b4f4030643ae92f7
Max Osborne <BurntAluminum@gmail.com>
2019-02-14 13:22:17 -0800

    This commit upgrades the calculator to include branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 var addThis = function(No1, No2) {
   return No1 + No2;
 };
@@ -11,16 +13,30 @@ var diviThis = function (No1, No2) {
 }
 
 var multiThis = function (No1, No2) {
-  return No1 / No2;
+  return No1 * No2;
 }
 
+//User interface
+
 $(document).ready(function() {
-  $("form#addThis").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var No1 = parseInt($("#addOne").val());
-    var No2 = parseInt($("#addTwo").val());
-    var result = addThis(No1, No2);
-    console.log(result);
+    var No1 = parseInt($("#input1").val());
+    var No2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: " + No1);
+    console.log("2nd number: " + No2);
+    console.log("operator: " + operator);
+    var result;
+    if (operator === "addition") {
+      result = addThis(No1, No2);
+    } else if (operator === "subtraction") {
+      result = subtrThis(No1, No2);
+    } else if (operator === "multiplication") {
+      result = multiThis(No1, No2);
+    } else if (operator === "division") {
+      result = diviThis(No1, No2);
+    }
     $("#answerThis").text(result);
     $("#Winestein").show();
   });
